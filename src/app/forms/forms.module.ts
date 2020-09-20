229fea9db1e126f982c6f625b83d21cd3c1ca15b
RaimundoOliveira <raimundopi025@gmail.com>
2020-09-20 20:20:31 -0300

    Included components and modules

---
+++ src/app/forms/forms.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormListComponent } from './form-list/form-list.component';
+import { FormEditComponent } from './form-edit/form-edit.component';
+import { ActiveComponent } from './shared/active/active.component';
+
+
+
+@NgModule({
+  declarations: [FormListComponent, FormEditComponent, ActiveComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class FormsModule { }
